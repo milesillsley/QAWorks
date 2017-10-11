08125934b28cb2da87a60a3c1e44c6124c3edb18
milesillsley <milesillsley@hotmail.co.uk>
2017-10-11 19:47:28 +0100

    Second Test
    Contact Us Page should submit a message with contact details

--- test/contact-page.js
+++ test/contact-page.js
@@ -1,9 +1,19 @@
 describe('Contact Us Page', function() {
-
     it('should have a Contact Us link on the homepage', function() {
         browser.url('/');
         var hasContactLink = browser.isExisting('=CONTACT US');
         expect(hasContactLink).toBe(true);
     });
-
+    describe('Valid contact Submission',function() {
+      it('should submit a message with contact details', function() {
+        browser.url('/');
+        browser.click('=CONTACT US');
+        browser.setValue('[name="your-name"]','J.Bloggs');
+        browser.setValue('[name="your-message"]','please contact me I want to find out more');
+        browser.setValue('[name="your-company"]','test automation');
+        browser.setValue('[name="your-email"]','j.Bloggs@qaworks.com');
+        var submission = browser.submitForm('#contact-us-send');
+        expect(submission.state).toBe('success');
+      });
+    });
 });
